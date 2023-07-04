90d1a31af0da068e0f78a641b3e8a4352d032a13
isaiasg09 <isaiashsobral@gmail.com>
2023-07-04 17:05:45 -0300

    web: made the storage of the access token with cookies

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,16 +1,27 @@
 import { api } from "@/lib/api";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+	// get code from query string
 	const { searchParams } = new URL(request.url);
 	const code = searchParams.get("code");
 
+	// send code to backend
 	const registerResponse = await api.post("/register", {
 		code
 	});
 
+	// get token from backend
 	const { token } = registerResponse.data;
 
-	console.log(token);
+	// redirect to home page with token in cookie
+	const redirectUrl = new URL("/", request.url);
+
+	const cookieExpiresInSeconds = 60 * 60 * 24 * 30; // 1 month
+
+	return NextResponse.redirect(redirectUrl, {
+		headers: {
+			"Set-Cookie": `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`
+		}
+	});
 }
-  
