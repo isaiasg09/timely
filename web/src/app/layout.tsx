46fc7c07926e92412513a0ada90a6b9bd8405c30
isaiasg09 <isaiashsobral@gmail.com>
2023-07-14 15:51:37 -0300

    web: made app layout and created new memory page route

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
 import "./globals.css";
 import { Roboto_Flex, Bai_Jamjuree as BaiJamjuree } from "next/font/google";
+import { Copyright } from "@/components/Copyright";
+import { Hero } from "@/components/Hero";
+import { Profile } from "@/components/Profile";
+import { SignIn } from "@/components/SignIn";
+import { cookies } from "next/headers";
 
 const baiJamjuree = BaiJamjuree({
 	subsets: ["latin"],
@@ -15,12 +20,37 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+	const isAuthenticated = cookies().has("token");
 	return (
 		<html lang="en">
 			<body
 				className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
 			>
-				{children}
+				<main className="grid min-h-screen grid-cols-2">
+					{/* Left */}
+					<div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars-l.svg)] bg-cover px-28 py-16">
+						{/* Blur */}
+						<div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+						{/* Stripes */}
+						<div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+						{/* Sign In or Profile*/}
+
+						{isAuthenticated ? <Profile /> : <SignIn />}
+
+						{/* Hero */}
+						<Hero />
+
+						{/* Copyright */}
+						<Copyright />
+					</div>
+
+					{/* Memories Page */}
+					<div className="flex flex-col bg-[url(../assets/bg-stars-r.svg)] bg-cover p-16">
+						{children}
+					</div>
+				</main>
 			</body>
 		</html>
 	);
--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 
 import nlwLogo from "../assets/nlw-spacetime-logo.svg";
+import { cookies } from "next/headers";
 
 export function Hero() {
+	const isAuthenticated = cookies().has("token");
+	const buttonHref = isAuthenticated
+		? "/memories/new"
+		: `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`;
+
 	return (
 		<div className="space-y-5">
 			<Image src={nlwLogo} alt="NLW SPACETIME" />
@@ -19,7 +25,7 @@ export function Hero() {
 			</div>
 
 			<a
-				href=""
+				href={buttonHref}
 				className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase	leading-none text-black transition hover:bg-green-600"
 			>
 				CADASTRAR LEMBRANÇA
